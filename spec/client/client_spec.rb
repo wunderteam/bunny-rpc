6a68e2a7f087fbdccacd0b198d650db4625a4665
Dave Riess <daveriess@gmail.com>
2016-02-05 14:45:19 -0700

    specify ruby version; hopefully CI will pick that up

--- spec/client/client_spec.rb
+++ spec/client/client_spec.rb
@@ -4,4 +4,17 @@ describe BunnyRPC::Client do
   it 'is a test spec' do
     expect(true).to eq(true)
   end
+
+  # describe 'moqueue' do
+  #   let(:mq) { MQ.new }
+  #   let(:queue) { queue = mq.queue('mocktacular') }
+  #   before do
+  #     topic = mq.topic("lolz")
+  #     queue.bind(topic, :key=> "cats.*")
+  #     queue.subscribe {|header, msg| puts [header.routing_key, msg]}
+  #     topic.publish("eatin ur foodz", :key => "cats.inUrFridge")
+  #   end
+  #
+  #   it { expect(queue.received_message?('eatin ur foodz')).to eq(true) }
+  # end
 end
